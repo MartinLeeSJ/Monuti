db0470f0f44fc7efbeec275ea3a8f9aae8daf6bf
MartinLeeSJ <tjr2922@gmail.com>
2023-03-16 12:59:51 +0900

    first commit

---
+++ GongdeunTop/GongdeunTop/GongdeunTopApp.swift
@@ -0,0 +1,17 @@
+//
+//  GongdeunTopApp.swift
+//  GongdeunTop
+//
+//  Created by Martin on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct GongdeunTopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
