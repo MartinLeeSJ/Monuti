79aff7435b9c244f4a9262de21b5d504af16f866
MartinLeeSJ <tjr2922@gmail.com>
2023-03-17 16:37:56 +0900

    [Feat] Timer View 남은 시간 부채꼴 모양

--- GongdeunTop/GongdeunTop/View/Home/TimerView.swift
+++ GongdeunTop/GongdeunTop/View/Home/TimerView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct TimerView: View {
+    let initialSecond: Int = 25 * 60
     @State private var remainSecond: Int = 25 * 60
     @State private var isRunning: Bool = false
     @State private var timer: Timer?
     
     private var minute: String {
-        var result: Int = Int(remainSecond / 60)
+        let result: Int = Int(remainSecond / 60)
         if result < 10 {
             return "0" + String(result)
         } else {
@@ -22,7 +23,7 @@ struct TimerView: View {
     }
     
     private var second: String {
-        var result: Int = remainSecond % 60
+        let result: Int = remainSecond % 60
         if result < 10 {
             return "0" + String(result)
         } else {
@@ -30,9 +31,13 @@ struct TimerView: View {
         }
     }
     
+    private var endDegree: Double {
+        return Double(remainSecond) / Double(initialSecond) * 360.0
+    }
+    
     
     var body: some View {
-        VStack {
+        VStack(alignment: .center) {
             HStack(spacing: 2) {
                 Text(minute)
                 Text(":")
@@ -46,6 +51,7 @@ struct TimerView: View {
                     timer?.invalidate()
                 } else {
                     timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                        print(endDegree)
                         if remainSecond > 0 {
                             remainSecond -= 1
                         }
@@ -55,9 +61,66 @@ struct TimerView: View {
             } label: {
                 Image(systemName: isRunning ?  "pause.fill" : "play.fill")
                     .font(.title)
+                    .foregroundColor(.gray)
             }
+            .overlay {
+                HStack {
+                    Button {
+                        timer?.invalidate()
+                        remainSecond = initialSecond
+                    } label: {
+                        Image(systemName: "chevron.backward.to.line")
+                            .font(.title)
+                            .foregroundColor(.gray)
+                    }
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "chevron.right.to.line")
+                            .font(.title)
+                            .foregroundColor(.gray)
+                    }
+
+                }
+                .frame(width: 160)
+            }
+            
+            
+            
+        }
+        .frame(width: 160)
+        .background {
+            Circle()
+                .foregroundColor(.GTyellowBright)
+                .frame(width: 300, height: 300)
+                .overlay {
+                    CircularSector(endDegree: endDegree)
+                        .foregroundColor(.GTyellow)
+                }
         }
     }
+    
+    
+}
+
+
+struct CircularSector: Shape {
+  let startDegree: Double = 270
+  var endDegree: Double
+  var clockwise: Bool = false
+
+  func path(in rect: CGRect) -> Path {
+    var path = Path()
+    let radius = max(rect.size.width, rect.size.height) / 2
+      path.move(to: CGPoint(x: rect.midX, y: rect.midY))
+      path.addArc(center: CGPoint(x: rect.midX, y: rect.midY),
+                  radius: radius,
+                  startAngle: Angle(degrees: startDegree),
+                  endAngle: Angle(degrees: endDegree + 270),
+                  clockwise: clockwise)
+      return path
+  }
 }
 
 struct TimerView_Previews: PreviewProvider {
