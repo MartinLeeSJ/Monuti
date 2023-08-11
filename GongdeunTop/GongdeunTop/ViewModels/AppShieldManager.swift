bac0f806cf6c94ff0a975c525289338e6771963d
MartinLeeSJ <tjr2922@gmail.com>
2023-08-11 15:50:04 +0900

    [Feat] 앱 차단항목 UserDefaults 에 저장

--- GongdeunTop/GongdeunTop/ViewModels/AppShieldManager.swift
+++ GongdeunTop/GongdeunTop/ViewModels/AppShieldManager.swift
@@ -6,18 +6,75 @@
 //
 
 import Foundation
+import Combine
 import FamilyControls
 import ManagedSettings
+import DeviceActivity
 
 class AppShieldManager: ObservableObject {
     @Published var activitySelection = FamilyActivitySelection(includeEntireCategory: true)
     @Published var isAppShieldOn: Bool = UserDefaults.standard.bool(forKey: "isAppShieldOn")
     
+    private let deviceActivityCenter = DeviceActivityCenter()
     private let concentrationStore = ManagedSettingsStore(named: .concentration)
+    
+    // UserDefaults Keys
     private let screentimeSettingKey: String = "isScreenTimeActivated"
+    private let activitySelectionkey: String = "activitySelection"
+    
+    private let encoder = PropertyListEncoder()
+    private let decoder = PropertyListDecoder()
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        self.activitySelection = savedActtivitySelection() ?? FamilyActivitySelection(includeEntireCategory: true)
+        
+        $activitySelection
+            .sink { [weak self] selection in
+                self?.saveActtivitySelection(selection)
+            }.store(in: &cancellables)
+    }
+    
+    private func saveActtivitySelection(_ selection: FamilyActivitySelection) {
+        let defaults = UserDefaults.standard
+        defaults.set( try? encoder.encode(selection), forKey: activitySelectionkey)
+    }
+    
+    private func savedActtivitySelection() -> FamilyActivitySelection? {
+        let defaults = UserDefaults.standard
+        
+        
+        guard let data = defaults.data(forKey: activitySelectionkey) else {
+            return nil
+        }
+
+        return try? decoder.decode(
+            FamilyActivitySelection.self,
+            from: data
+        )
+    }
     
     func setAppShield(newValue bool: Bool) {
         UserDefaults.standard.set(bool, forKey: "isAppShieldOn")
+    }
+    
+    private func startMonitoring() throws {
+        let event = DeviceActivityEvent(applications: activitySelection.applicationTokens,
+                                        categories: activitySelection.categoryTokens,
+                                        webDomains: activitySelection.webDomainTokens,
+                                        threshold: DateComponents(day: 1))
+        let schedule = DeviceActivitySchedule(
+            intervalStart: DateComponents(hour: 0, minute: 0, second: 0),
+            intervalEnd: DateComponents(hour: 23, minute: 59, second: 59),
+            repeats: true
+        )
+    
+        try deviceActivityCenter.startMonitoring(.concentration, during: schedule, events: [.doNotDisturb : event])
+    }
+    
+    private func stopMonitoring() {
+        deviceActivityCenter.stopMonitoring()
     }
     
     
@@ -25,14 +82,21 @@ class AppShieldManager: ObservableObject {
     func startConcentrationAppShield() {
         guard isAppShieldOn else { return }
         print("::::::Shield is Operating")
-        dump(activitySelection.applicationTokens)
         concentrationStore.shield.applications = activitySelection.applicationTokens
+        
+        do {
+           try startMonitoring()
+        } catch {
+            print("Error while monitoring:::\(error.localizedDescription)")
+        }
+        
     }
     
     func stopConcentrationAppShield() {
         guard isAppShieldOn else { return }
         print("::::::Stop Sheilding")
         concentrationStore.clearAllSettings()
+        stopMonitoring()
     }
     
 }
@@ -41,3 +105,10 @@ extension ManagedSettingsStore.Name {
     static let concentration = Self("concentration")
 }
 
+extension DeviceActivityName {
+    static let concentration = Self("concentration")
+}
+
+extension DeviceActivityEvent.Name {
+    static let doNotDisturb = Self("doNotDisturb")
+}
