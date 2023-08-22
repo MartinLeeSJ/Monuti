b972d071b71ddc8ebad1874255f815a3a8a6a577
MartinLeeSJ <tjr2922@gmail.com>
2023-08-22 20:07:33 +0900

    [Feat #103] 세팅뷰 버튼 순서 변경

--- GongdeunTop/GongdeunTop/Views/Setting/SettingView.swift
+++ GongdeunTop/GongdeunTop/Views/Setting/SettingView.swift
@@ -12,6 +12,7 @@ import FamilyControls
 
 enum SheetType: Identifiable {
     case color
+    
     var id: Self { self }
 }
 
@@ -19,6 +20,7 @@ struct SettingView: View {
     @EnvironmentObject var authManager: AuthManager
     @EnvironmentObject var appBlockManager: AppBlockManager
     @State private var sheetType: SheetType?
+    @State private var isSignOutPresented: Bool = false
     
     private let center = AuthorizationCenter.shared
     
@@ -28,15 +30,10 @@ struct SettingView: View {
                 Button {
                     sheetType = .color
                 } label: {
-                    Text("ColorSetting")
+                    Text("ColorThemeSetting")
                 }
-                
-                Button {
-                    authManager.signOut()
-                } label: {
-                    Text("SignOut")
-                }
-                
+                .tint(.basicFontColor)
+
                 Section {
                     Toggle("집중 시 다른 앱 차단",
                            isOn: $appBlockManager.isAppBlockOn.animation())
@@ -57,6 +54,30 @@ struct SettingView: View {
                     isPresented: $appBlockManager.isActivitySelectionPickerOn,
                     selection: $appBlockManager.activitySelection)
                 
+                Section {
+                    Button {
+                        isSignOutPresented = true
+                    } label: {
+                        Text("SignOut")
+                    }
+                    .tint(.basicFontColor)
+                    .confirmationDialog("SignOut", isPresented: $isSignOutPresented) {
+                        Button(role: .cancel) {
+                            isSignOutPresented = false
+                        } label: {
+                            Text("Cancel")
+                        }
+                        
+                        Button(role: .destructive) {
+                            authManager.signOut()
+                        } label: {
+                            Text("SignOut")
+                        }
+                    } message: {
+                        Text("really_signOut")
+                    }
+                }
+                
             }
             .sheet(item: $sheetType) { type in
                 switch type {
@@ -65,6 +86,7 @@ struct SettingView: View {
                         .presentationDetents([.fraction(0.2)])
                 }
             }
+            
         }
     }
 }
